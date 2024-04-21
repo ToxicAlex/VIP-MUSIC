2941b3f2dee3d442cef4fbf23efd718bdae773c9
ToxicAlex <166980254+ToxicAlex@users.noreply.github.com>
2024-04-21 18:19:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/JAI_SHREE_RAM_CHANNEL")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/T0XIC_CLUB")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/JSR_CHATTING")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/JAI_SHREE_RAM_CHATTING")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
